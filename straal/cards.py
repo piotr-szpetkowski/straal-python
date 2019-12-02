cf9d35d01ce9ec9a2ddbf5eaf2d5916025767ced
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2019-12-03 00:46:47 +0100

    Initial Card schema definition

--- straal/cards.py
+++ straal/cards.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 from straal.base import ApiObject
-from straal.customers import Customer
 
 
 @dataclass
@@ -9,6 +8,20 @@ class Card(ApiObject):
     RESOURCE_CREATE_URI = "/v1/customers/{customer_id}/cards"
     RESOURCE_DETAIL_URI = "/v1/cards/{idx}"
     RESOURCE_LIST_URI = "/v1/cards"
+    id: str
+    created_at: int
+    state: str
+    state_flags: list
+    transactions: list
+    brand: str
+    name: str
+    num_bin: str
+    num_last_4: str
+    expiry_month: int
+    expiry_year: int
+    origin_ipaddr: str
+    customer: dict
+    extra_data: dict
 
     @classmethod
     def create(

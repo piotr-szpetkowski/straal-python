d576cac24914e7d2c84ff51709be4280edb7fb88
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2019-12-03 19:27:54 +0100

    Update Customer.created_at type to datetime

--- straal/customers.py
+++ straal/customers.py
@@ -1,3 +1,4 @@
+import datetime
 from dataclasses import dataclass, field
 from typing import List, Optional
 
@@ -10,10 +11,13 @@ class Customer(ApiObject):
     RESOURCE_DETAIL_URI = "/v1/customers/{idx}"
     RESOURCE_LIST_URI = "/v1/customers"
     id: str
-    created_at: int
     email: str
     reference: str
+    created_at: datetime.datetime = field(repr=False)
     last_transaction: Optional[dict] = field(default=None, repr=False)
+
+    def __post_init__(self):
+        self.created_at = datetime.datetime.utcfromtimestamp(self.created_at)
 
     @classmethod
     def create(cls, email: str, reference: str) -> "Customer":

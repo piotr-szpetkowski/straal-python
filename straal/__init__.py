ea3dccc662d6b14fafad9bee03c78ad0ac7a9dcc
Piotr Szpetkowski <piotr.szpetkowski@pyquest.space>
2020-01-30 19:59:55 +0100

    Add missing exports to __all__

--- straal/__init__.py
+++ straal/__init__.py
@@ -1,4 +1,4 @@
 from straal.base import get_api_key, get_base_url, init
 from straal.customers import Customer
 
-__all__ = ["init", "Customer"]
+__all__ = ["get_api_key", "get_base_url", "init", "Customer"]
